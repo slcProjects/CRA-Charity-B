f3e8221a65f3864743572c7a4af757aff24ef42d
CGraves <cgraves@sl.on.ca>
2023-08-22 14:28:05 -0400

    developed by

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -20,7 +20,7 @@ export default class MainScene extends Phaser.Scene {
     image.setPosition(this.cameras.main.centerX, this.cameras.main.centerY);
     image.setScale(this.cameras.main.width / image.width, this.cameras.main.height / image.height);
     const title = new TitleLabel(this,50,80,"Escape From Mars!", 120,50, 60, 2);
-    const names = new TitleLabel(this,50,260, "By: St. Lawrence College, CPA",10,10, 20,0)
+    const names = new TitleLabel(this,50,260, "Developed by: St. Lawrence College, CPA",10,10, 20,0)
     const startButton = new Button(this,230,260,'Begin',"instructions");
     const engButton = new LanguageButton(this, 130,200, 'English', "eng");
     const frButton = new LanguageButton(this, 330,200, 'Francais', "fr");
